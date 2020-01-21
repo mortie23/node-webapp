77d8f1b68151871ef824baea4f242202daad365f
Christopher Mortimer <christopher@mortimer.xyz>
2020-01-21 08:43:57 +0800

    write to server

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+
+const fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+
+console.log('test');
+
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+	custom = 'test\n',
+        response = [message, version, custom].join('\n');
     res.end(response);
 });
 server.listen();
